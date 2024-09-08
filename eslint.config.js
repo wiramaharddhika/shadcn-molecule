2ee8a48c8ee01f2b31266b3e5234a62f12b51198
wiramaharddhika <wiramaharddhika@gmail.com>
2024-09-08 14:24:26 +0800

    chore: config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +17,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +25,13 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': ['error', {
+        "groups": [["^\\u0000", "^@?\\w", "^[^.]", "^\\."]]
+      }],
+      'simple-import-sort/exports': 'error',
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'quotes': ['error', 'single'],
     },
   },
-)
+);
