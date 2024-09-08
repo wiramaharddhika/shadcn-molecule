ae74d58dbdad870d2d9f2b3f0b736e4cf9286a56
wiramaharddhika <wiramaharddhika@gmail.com>
2024-09-08 14:42:51 +0800

    chore: setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
